c5a61a69c0bbe172c3c60d4b56df13da8816faf0
ChiCodex <kaylafrancisca420@gmail.com>
2023-09-22 07:17:31 -0700

    updated printf_func.c

--- printf_func.c
+++ printf_func.c
@@ -3,9 +3,10 @@
 /**
  * _printf - This function replicates the standard printf function.
  * @format: This represents the string input.
- * ...: This is the variadic function representing the number of arguments passed.
- * Return: The total number of printed characters (charcount) 
- *         Or -1 if string input is NULL.
+ * @...: This is the variadic function
+ *	representing the number of arguments passed.
+ * Return: The total number of printed characters (charcount)
+ *	 Or -1 if string input is NULL.
  */
 
 int _printf(const char *format, ...)
